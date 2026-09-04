fe8a5fee8ceddca89d09c7de5e9a8e23230bc985
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-09-04 10:58:56 -0300

    feat: Criando a Service dos Clientes

---
+++ demo/src/main/java/com/autoCenterSilva/demo/service/ClientesService.java
@@ -0,0 +1,45 @@
+package com.autoCenterSilva.demo.service;
+
+import com.autoCenterSilva.demo.dto.request.ClienteCreatRequest;
+import com.autoCenterSilva.demo.dto.response.ClienteCreateResponse;
+import com.autoCenterSilva.demo.entity.Cliente;
+import com.autoCenterSilva.demo.repository.ClientesRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientesService {
+    private final ClientesRepository clientesRepository;
+
+    public ClientesService(ClientesRepository clientesRepository) {
+        this.clientesRepository = clientesRepository;
+    }
+
+    public List<Cliente> findAll() {
+        return clientesRepository.findAll();
+    }
+
+    public Optional<Cliente> findById(Long id) {
+        return clientesRepository.findById(id);
+    }
+
+    @Transactional
+    public ClienteCreateResponse salvarCliente(ClienteCreatRequest clienteCreatRequest){
+        if (!clienteCreatRequest.getSenha().equals(clienteCreatRequest.getConfirmarSenha())) {
+            throw new IllegalArgumentException("As senhas não coincidem");
+        }
+        Cliente cliente = new Cliente();
+        cliente.setNome(clienteCreatRequest.getNome());
+        cliente.setTelefone(clienteCreatRequest.getTelefone());
+        cliente.setSenha( clienteCreatRequest.getSenha());
+        cliente.setStatus(true);
+
+        Cliente clienteSalvo = this.clientesRepository.save(cliente);
+        return ClienteCreateResponse.de(clienteSalvo);
+    }
+
+
+}
