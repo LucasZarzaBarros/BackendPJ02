b09d3ef7aba517ca048b844a4cc3c02f180750cb
Lucas Eduardo <lucaszarzabarros@gmail.com>
2026-09-15 10:08:44 -0300

    feat: Adicionando as validações da DTO de Login do Cliente

--- demo/src/main/java/com/autoCenterSilva/demo/dto/request/ClienteLoginRequest.java
+++ demo/src/main/java/com/autoCenterSilva/demo/dto/request/ClienteLoginRequest.java
@@ -1,12 +1,20 @@
 package com.autoCenterSilva.demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ClienteLoginRequest {
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "Telefone é obrigatório")
     private String telefone;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, max = 20, message = "Senha deve ter entre 6 e 20 caracteres")
     private String senha;
 }
